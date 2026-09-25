16ff1779835eb278028818f661b631f45ba90826
Qoder Developer <qoder@example.com>
2026-09-25 21:48:25 +0800

    统一两套皮肤的回本金额样式

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'erjie-vault-static-';
-const CACHE_NAME = `${CACHE_PREFIX}v11`;
+const CACHE_NAME = `${CACHE_PREFIX}v12`;
 const ASSETS = ['./', './index.html', './style.css', './app.js', './ledger.js', './theme.js', './icon.svg', './icon-berry.svg', './icon.png', './manifest.webmanifest'];
 
 self.addEventListener('install', event => {
